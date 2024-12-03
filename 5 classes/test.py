05ca267e6bc7e196e0e23a556ed4b7ac453b9994
ahmadsh2007 <shatnawiahmad2007@gmail.com>
2024-12-03 12:58:13 +0300

    Optimized the Project

--- 5 classes/test.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def removeDuplicates(self, nums: list[int]) -> int:
-        nums[:] = list(set(nums))
-       return len(list(set(nums)))
-    
-solution = Solution()
-nums = [0,0,1,1,1,2,2,3,3,4]
-print(solution.removeDuplicates(nums))
